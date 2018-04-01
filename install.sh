48cf473f46de5762d0b2327452c211ea54700fc9
Øystein Bjørndal <obtitus@gmail.com>
2018-04-01 13:52:01 +0200

    added a few missing languages, now raises exception on missing languages, do not remove :lang suffix for multi-linguale places

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git clone https://github.com/obtitus/py_import_utility_to_osm
 mv py_import_utility_to_osm utility_to_osm
 cd utility_to_osm
 git clone https://github.com/xificurk/osmapis
+cp osmapis/osmapis.py .
