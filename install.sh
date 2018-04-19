91e527bb07d9f468556e4938b71ae8865870971d
Øystein Bjørndal <obtitus@gmail.com>
2018-04-19 21:53:57 +0200

    bugfix in install

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+
+pip install -r requirements.txt
+
 # yeah, not very pythonic I know...
 git clone https://github.com/obtitus/py_import_utility_to_osm
 mv py_import_utility_to_osm utility_to_osm
 cd utility_to_osm
 git clone https://github.com/xificurk/osmapis
 cp osmapis/osmapis.py .
-
-pip install -r requirements.txt
