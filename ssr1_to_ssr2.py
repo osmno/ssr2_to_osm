3b32e338ca81b91fa508d7e11a934412536136b7
Øystein Bjørndal <obtitus@gmail.com>
2018-05-01 11:37:57 +0200

    changes to be able to run Source_id_gammel.osm and changes to the duplicate fixme to only warn on actuall duplicates

--- ssr1_to_ssr2.py
+++ ssr1_to_ssr2.py
@@ -9,14 +9,26 @@ filename_gammel_converted = '/Users/ob/Google Drive/ssr2_to_osm_data/Nordland_ga
 conversion_table_filename = '/Users/ob/Google Drive/Stedsnavn/Konvertering fra SSR1_id til SSR2_id.xlsx'
 filenames_new = ['/Users/ob/Programming/Python/osm/ssr2_to_osm/html/data/1837/1837.osm',
                  '/Users/ob/Programming/Python/osm/ssr2_to_osm/html/data/1838/1838.osm']
-tags_to_remove = ['no-kartverket-ssr:date', 'no-kartverket-ssr:objid', 'no-kartverket-ssr:url']
+
+COMPARE_TAGS = False
+# filename_gammel = '/Users/ob/Google Drive/Stedsnavn/Source_id_gammel.osm'
+# filename_gammel_converted = '/Users/ob/Google Drive/ssr2_to_osm_data/Source_id_gammel_converted.osm'
+# conversion_table_filename = '/Users/ob/Google Drive/Stedsnavn/Konvertering fra SSR1_id til SSR2_id.xlsx'
+# filenames_new = list()
+# for kommune_nr in (2022, 2023, 632, 631, 621, 623, 628, 627, 602, 625, 624, 604):
+#     filenames_new.append('/Users/ob/Programming/Python/osm/ssr2_to_osm/html/data/%04d/%04d.osm' % (kommune_nr, kommune_nr))
+
+tags_to_remove = ['no-kartverket-ssr:date', 'no-kartverket-ssr:objid', 'no-kartverket-ssr:url',
+                  'source_id', 'source_ref']
 tags_to_ignore_in_comparision = ['fixme', 'place']
+tags_to_ignore_in_comparision.extend(tags_to_remove)
 
 with open(filename_gammel, 'r') as f:
     osm_g = osmapis.OSM.from_xml(f.read())
 # Create dictionary with 'no-kartverket-ssr:objid' as key
 osm_g_dict = dict()
 for item_g in osm_g:
+    ref_from_url = None
     # if 'no-kartverket-ssr:objid' in item_g.tags:
     #     value = item_g.tags['no-kartverket-ssr:objid']
     #     osm_g_dict[value] = item_g
@@ -28,6 +40,23 @@ for item_g in osm_g:
             osm_g_dict[value] = item_g
         else:
             print('error: unable to find enhet in url = %s', url)
+    if 'source_ref' in item_g.tags:
+        # fixme: copy of above
+        url = item_g.tags['source_ref']
+        reg = re.search('enhet=(\d+)', url)
+        if reg:
+            ref_from_url = reg.group(1)
+        else:
+            print('error: unable to find enhet in url = %s', url)
+        
+    if 'source_id' in item_g.tags:
+        value = item_g.tags['source_id']
+        if ref_from_url != None:
+            if value != ref_from_url:
+                print('expected enhet from url and source_id to be identical "%s" != "%s", skipping' % (value, ref_from_url))
+                continue
+        
+        osm_g_dict[value] = item_g
 
 print('read osm file, len(elements) = %s, %s items with no-kartverket-ssr:objid' % (len(osm_g), len(osm_g_dict)))
 
@@ -69,7 +98,6 @@ def compare_tag(osm_item, ssr2_item, key):
     fixme = []
     missing = False
     if key not in osm_item:
-        #if key == 'water' and 
         fixme.append('osm: %s = "%s" missing from old import' % (key, ssr2_item[key]))
         missing = True
     if key not in ssr2_item:
@@ -80,6 +108,17 @@ def compare_tag(osm_item, ssr2_item, key):
         fixme.append('value differs osm: %s = "%s" != ssr2: %s = "%s"' % (key, osm_item[key], key, ssr2_item[key]))
 
     return fixme
+
+def remove_names_already_present(list_of_names, dct):
+    list_of_names = list(list_of_names)
+    for key in dct:
+        if key.endswith('name'):
+            try:
+                ix = list_of_names.index(dct[key])
+                del list_of_names[ix]
+            except ValueError:
+                pass
+    return list_of_names
 
 for ssrid in osm_g_dict:
     item_g = osm_g_dict[ssrid]
@@ -105,39 +144,92 @@ for filename in filenames_new:
         if 'ssr:stedsnr' in item.tags:
             new_tag = item.tags['ssr:stedsnr']
             if new_tag in stedsnummer_2_ssrid:
-                old_tag = stedsnummer_2_ssrid[new_tag]
+                old_tags = stedsnummer_2_ssrid[new_tag]
             else:
                 print('conversion not found for ssr:stedsnr = %s' % (new_tag))
                 continue
 
             fixme = []
-            if len(old_tag) > 1:
+            if len(old_tags) != 1:
+                #fixme.append('duplicate conversion stedsnummer found in conversion table, fix manually')
+                print('duplicate conversion stedsnummer found in conversion table:')
+                for value in old_tags:
+                    print('stedsnummer = %s - ssrid = %s' % (value, new_tag))
+                    #fixme.append('stedsnummer = %s - ssrid = %s' % (value, new_tag))
+                
+                #old_tag = ';'.join(old_tags)                    
+
+            item_g_lst = list()                
+            for old_tag in old_tags:
+                # Look for old_tag in osm_g
+                
+                if old_tag in osm_g_dict:
+                    item_g = osm_g_dict[old_tag] # found it
+                    item_g_lst.append(item_g)
+                
+            if len(item_g_lst) == 0:
+                continue
+            elif len(item_g_lst) != 1:
                 fixme.append('duplicate conversion stedsnummer found in conversion table, fix manually')
-                for value in old_tag:
+                for item_g in item_g_lst:
                     fixme.append('stedsnummer = %s - ssrid = %s' % (value, new_tag))
 
-            old_tag = old_tag[0]
-            
-            # Look for old_tag in osm_g
-            if old_tag in osm_g_dict:
-                item_g = osm_g_dict[old_tag] # found it
+            for item_g in item_g_lst:
                 item_g.tags['ssr:stedsnr'] = new_tag
                 item_g.attribs['action'] = 'modify'
 
-                for tag in item.tags:
-                    if not(tag.startswith('ssr')) and (tag not in tags_to_ignore_in_comparision):
-                        fixme.extend(compare_tag(item_g.tags, item.tags, tag))
-                
+                # Get list of alt_names and names in ssr2
+                names = list()
+                for name_key in ('name', 'alt_name'):
+                    try:
+                        value = item.tags[name_key]
+                        value_split_semi = value.split(';')
+                        value_split = list()
+                        for semi_item in value_split_semi:
+                            value_split.extend(semi_item.split(' - '))
+                        names.extend(value_split)
+                    except KeyError:
+                        pass
+                # Remove names already imported:
+                names = remove_names_already_present(names, item_g.tags)
+
+                # Only add if missing alt_name
+                if 'alt_name' not in item_g.tags and len(names) != 0:
+                    # Ok, add to alt_name:
+                    item_g.tags['alt_name'] = ';'.join(names)
+
+                # Add loc_name and old_name if missing
+                for name_key in ('loc_name', 'old_name'):
+                    if name_key not in item_g.tags:
+                        try:
+                            value = item.tags[name_key]
+                            # Ensure value not already in any other name tag
+                            value_lst = remove_names_already_present([value], item_g.tags)
+                            if len(value_lst) == 1:
+                                # ok, guess we are safe
+                                item_g.tags[name_key] = value_lst[0]
+                        except KeyError:
+                            pass
+
+                if COMPARE_TAGS:
+                    for tag in item.tags:
+                        if not(tag.startswith('ssr')) and (tag not in tags_to_ignore_in_comparision):
+                        #if tag.endswith(('name', 'alt_name')):
+                            fixme.extend(compare_tag(item_g.tags, item.tags, tag))
+                # for tag in item_g.tags:
+                #     if tag.endswith(('name', 'alt_name')):
+                #         fixme.extend(compare_tag(item_g.tags, item.tags, tag))
+
                 if len(fixme) != 0:
                     prev_fixme = item_g.tags.pop('fixme', '')
                     if prev_fixme != '':
                         fixme.insert(0, prev_fixme)
-                    
+
                     item_g.tags['fixme'] = ';'.join(fixme)
 
-                    for rm_tag in tags_to_remove:
-                        item_g.tags.pop(rm_tag, '')
-                    
+                for rm_tag in tags_to_remove:
+                    item_g.tags.pop(rm_tag, '')
+
             else:
                 #print('')
                 continue
